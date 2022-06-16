b3ea9c18d5084c154471b361f8f78ab361097859
Richard Huang <flyerhzm@gmail.com>
2022-06-17 06:53:27 +0800

    export NodeMutation and errors

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import NodeMutation from "./node-mutation";
+import { NotSupportedError, ConflictActionError } from "./error";
+
+export default NodeMutation;
+export { NotSupportedError, ConflictActionError };
