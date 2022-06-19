32ec48de3a18fc25a54844a74834dc7e0a3f23e2
Richard Huang <flyerhzm@gmail.com>
2022-06-19 09:29:16 +0800

    export more

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import NodeMutation from "./node-mutation";
+import NodeMutation, { STRATEGY } from "./node-mutation";
 import { NotSupportedError, ConflictActionError } from "./error";
+import { InsertOptions, ReplaceWithOptions, ReplaceOptions } from "./types";
 
 export default NodeMutation;
-export { NotSupportedError, ConflictActionError };
+export { STRATEGY, NotSupportedError, ConflictActionError, InsertOptions, ReplaceWithOptions, ReplaceOptions };
