ac45d0835f6671ae581c132d8aea09f211638dfc
Richard Huang <flyerhzm@gmail.com>
2022-06-20 22:47:26 +0800

    fix comment

--- test/helper.ts
+++ test/helper.ts
@@ -1,8 +1,8 @@
 import ts from "typescript";
 
-// Parse source code and return typescript SourceFile.
+// Parse source code and return the first typescript statement.
 // @param code [String] source code.
-// @return [ts.SourceFile] typescrpt SourceFile node.
+// @return [ts.Node] first typescrpt statement node.
 export const parseCode = (code: string): ts.Node => {
   return ts.createSourceFile("code.ts", code, ts.ScriptTarget.Latest, true).statements[0];
 };
