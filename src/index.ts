f6efa5679d4678c0a7b04fd3647ead05177c36dc
Richard Huang <flyerhzm@gmail.com>
2022-08-10 16:37:56 +0800

    export TypescriptAdapter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import NodeMutation, { STRATEGY } from "./node-mutation";
 import Adapter from "./adapter";
+import TypescriptAdapter from "./typescript-adapter";
 import { NotSupportedError, ConflictActionError } from "./error";
 import { InsertOptions, ReplaceWithOptions, ReplaceOptions, ProcessResult } from "./types";
 
 export default NodeMutation;
 export {
   Adapter,
+  TypescriptAdapter,
   STRATEGY,
   NotSupportedError,
   ConflictActionError,
