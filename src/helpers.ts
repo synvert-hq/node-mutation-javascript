341b461b58f369be72cd23d5cc097d0cc652f97f
Richard Huang <flyerhzm@gmail.com>
2023-09-26 20:02:47 +0800

    add iterateActions helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,4 @@
+import type { Action } from './types/action';
 
 import NodeMutation from "./node-mutation";
 import Adapter from "./adapter";
@@ -8,4 +9,14 @@ import Adapter from "./adapter";
  */
 export function getAdapter<T>(): Adapter<T> {
   return NodeMutation.getAdapter();
+}
+
+export function iterateActions(actions: Action[], callback: (action: Action) => void) {
+  actions.forEach((action) => {
+    if (action.type === 'group') {
+      iterateActions(action.actions!, callback);
+    } else {
+      callback(action);
+    }
+  });
 }
