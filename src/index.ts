b8406ed26f65dca4bf624e18691c83a89f76d0fe
Richard Huang <flyerhzm@gmail.com>
2023-10-15 10:20:51 +0800

    export RemoveOptions

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import TypescriptAdapter from "./adapter/typescript";
 import GonzalesPeAdapter from "./adapter/gonzales-pe";
 import EspreeAdapter from "./adapter/espree";
 import { NotSupportedError, ConflictActionError } from "./error";
-import type { InsertOptions, ReplaceOptions, DeleteOptions } from "./types/action";
+import type { InsertOptions, ReplaceOptions, DeleteOptions, RemoveOptions } from "./types/action";
 import type { ProcessResult, TestResult } from "./types/node-mutation";
 
 export default NodeMutation;
@@ -20,6 +20,7 @@ export {
   InsertOptions,
   ReplaceOptions,
   DeleteOptions,
+  RemoveOptions,
   ProcessResult,
   TestResult
 };
