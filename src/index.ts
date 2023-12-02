f50c770c6a858cf95f5c9e4f44104c752a09acee
Richard Huang <flyerhzm@gmail.com>
2023-12-02 13:11:11 +0800

    Revert "no need to expose TypescriptAdapter, EspreeAdapter and GonzalesAdapter"

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import NodeMutation from "./node-mutation";
 import Adapter from "./adapter";
 import Strategy from "./strategy";
+import TypescriptAdapter from "./adapter/typescript";
+import GonzalesPeAdapter from "./adapter/gonzales-pe";
+import EspreeAdapter from "./adapter/espree";
 import { NotSupportedError, ConflictActionError } from "./error";
 import type { InsertOptions, ReplaceOptions, DeleteOptions, RemoveOptions } from "./types/action";
 import type { ProcessResult, TestResult } from "./types/node-mutation";
@@ -8,6 +11,9 @@ import type { ProcessResult, TestResult } from "./types/node-mutation";
 export default NodeMutation;
 export {
   Adapter,
+  TypescriptAdapter,
+  EspreeAdapter,
+  GonzalesPeAdapter,
   Strategy,
   NotSupportedError,
   ConflictActionError,
